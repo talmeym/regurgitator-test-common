017d88db27b8db2b96d14049ee4b98c675f8932e
talmeym <mtalmey@gmail.com>
2015-12-07 11:37:14 +0000

    tidied logging

--- src/main/java/com/emarte/regurgitator/core/Caching.java
+++ src/main/java/com/emarte/regurgitator/core/Caching.java
@@ -1,8 +1,7 @@
 package com.emarte.regurgitator.core;
 
 public class Caching {
-	private static Log log = Log.getLog(Caching.class);
-
+	private static final Log log = Log.getLog(Caching.class);
 	private static final CacheProvider PROVIDER = new DefaultCacheProvider();
 
 	public static <TYPE> Cache<TYPE> getCache(Class<TYPE> clazz) {
