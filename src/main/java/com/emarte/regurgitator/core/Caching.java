ae3461e7982535dcda1e13a07a4788298439ba92
talmeym <mtalmey@gmail.com>
2015-12-09 12:16:51 +0000

    tidied caching

--- src/main/java/com/emarte/regurgitator/core/Caching.java
+++ src/main/java/com/emarte/regurgitator/core/Caching.java
@@ -10,4 +10,26 @@ public class Caching {
 		log.debug("Using default cache");
 		return PROVIDER.getCache(clazz);
 	}
+
+	public static class DefaultCacheProvider implements CacheProvider {
+		@Override
+		public <TYPE> Cache<TYPE> getCache(Class<TYPE> clazz) {
+			return new Cache<TYPE>() {
+				@Override
+				public boolean contains(Object key) {
+					return false;
+				}
+
+				@Override
+				public TYPE get(Object key) {
+					throw new UnsupportedOperationException("Caching not implemented for tests");
+				}
+
+				@Override
+				public void set(Object key, Object value) {
+					// do nothing
+				}
+			};
+		}
+	}
 }
