8bb3c1fc229b74a4d471696d62d57a78ee2c9263
talmeym <mtalmey@gmail.com>
2015-12-17 10:37:10 +0000

    added ValueSource to handle param sources with static defaults

--- src/main/java/com/emarte/regurgitator/extensions/FreemarkerProcessor.java
+++ src/main/java/com/emarte/regurgitator/extensions/FreemarkerProcessor.java
@@ -3,7 +3,7 @@ package com.emarte.regurgitator.extensions;
 import com.emarte.regurgitator.test.Mock;
 
 public class FreemarkerProcessor extends Mock {
-	public FreemarkerProcessor(String staticValue) {
-		super(staticValue);
+	public FreemarkerProcessor(String value) {
+		super(value);
 	}
 }
--- src/main/java/com/emarte/regurgitator/extensions/VelocityProcessor.java
+++ src/main/java/com/emarte/regurgitator/extensions/VelocityProcessor.java
@@ -3,7 +3,7 @@ package com.emarte.regurgitator.extensions;
 import com.emarte.regurgitator.test.Mock;
 
 public class VelocityProcessor extends Mock {
-	public VelocityProcessor(String staticValue) {
-		super(staticValue);
+	public VelocityProcessor(String value) {
+		super(value);
 	}
 }
