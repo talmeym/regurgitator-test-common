be697b86a06cacae5d352f63768a8c68c7534e4b
talmeym <mtalmey@gmail.com>
2016-01-06 15:03:38 +0000

    renamed http-call-through as http-call

--- src/main/java/com/emarte/regurgitator/extensions/web/HttpCall.java
+++ src/main/java/com/emarte/regurgitator/extensions/web/HttpCall.java
@@ -3,8 +3,8 @@ package com.emarte.regurgitator.extensions.web;
 import com.emarte.regurgitator.core.Step;
 import com.emarte.regurgitator.test.MockStep;
 
-public class HttpCallThrough extends MockStep {
-	public HttpCallThrough(Object id, HttpMessageProxy httpMessageProxy, Step responseProcessing) {
+public class HttpCall extends MockStep {
+	public HttpCall(Object id, HttpMessageProxy httpMessageProxy, Step responseProcessing) {
 		super(id, httpMessageProxy, responseProcessing);
 	}
 }
