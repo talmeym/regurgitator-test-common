eba0069169484e5cd1c9e1def07b9db585ac7837
talmeym <mtalmey@gmail.com>
2016-01-07 09:38:53 +0000

    improved logging of http message proxy and added params attr to isolate step

--- src/main/java/com/emarte/regurgitator/core/Isolate.java
+++ src/main/java/com/emarte/regurgitator/core/Isolate.java
@@ -3,7 +3,7 @@ package com.emarte.regurgitator.core;
 import com.emarte.regurgitator.test.MockStep;
 
 public class Isolate extends MockStep {
-	public Isolate(Object id, Step step, boolean includeSession) {
-		super(id, step, includeSession);
+	public Isolate(Object id, Step step, boolean includeSession, boolean includeParameters) {
+		super(id, step, includeSession, includeParameters);
 	}
 }
