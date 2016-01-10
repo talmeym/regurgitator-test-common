fa15187e97bf6abeed7ca516474e36750dd305fa
talmeym <mtalmey@gmail.com>
2016-01-10 14:15:56 +0000

    allowed http call and isolate steps to hold multiple children

--- src/main/java/com/emarte/regurgitator/core/Isolate.java
+++ src/main/java/com/emarte/regurgitator/core/Isolate.java
@@ -2,8 +2,10 @@ package com.emarte.regurgitator.core;
 
 import com.emarte.regurgitator.test.MockStep;
 
+import java.util.List;
+
 public class Isolate extends MockStep {
-	public Isolate(Object id, Step step, boolean includeSession, boolean includeParameters) {
-		super(id, step, includeSession, includeParameters);
+	public Isolate(Object id, List<Step> steps, boolean includeSession, boolean includeParameters) {
+		super(id, steps, includeSession, includeParameters);
 	}
 }
--- src/main/java/com/emarte/regurgitator/extensions/web/HttpCall.java
+++ src/main/java/com/emarte/regurgitator/extensions/web/HttpCall.java
@@ -3,8 +3,10 @@ package com.emarte.regurgitator.extensions.web;
 import com.emarte.regurgitator.core.Step;
 import com.emarte.regurgitator.test.MockStep;
 
+import java.util.List;
+
 public class HttpCall extends MockStep {
-	public HttpCall(Object id, HttpMessageProxy httpMessageProxy, Step responseProcessing) {
-		super(id, httpMessageProxy, responseProcessing);
+	public HttpCall(Object id, HttpMessageProxy httpMessageProxy, List<Step> steps) {
+		super(id, httpMessageProxy, steps);
 	}
 }
