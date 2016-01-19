1f167a5b219403b9ceeafcbba06f0d83e40af8ed
talmeym <mtalmey@gmail.com>
2016-01-19 15:44:53 +0000

    added create-http-response

--- src/main/java/com/emarte/regurgitator/core/CreateResponse.java
+++ src/main/java/com/emarte/regurgitator/core/CreateResponse.java
@@ -2,7 +2,7 @@ package com.emarte.regurgitator.core;
 
 import com.emarte.regurgitator.test.MockStep;
 
-class CreateResponse extends MockStep {
+public class CreateResponse extends MockStep {
 	CreateResponse(String id, ValueSource valueSource, ValueProcessor processor) {
 		super(id, valueSource, processor);
 	}
