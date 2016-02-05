abbb25f7ee6190d0a3f1d73b2e9a86c0df1f7312
talmeym <mtalmey@gmail.com>
2016-02-05 17:11:50 +0000

    tidied up http message proxy

---
+++ src/main/java/com/emarte/regurgitator/extensions/web/HttpClientWrapper.java
@@ -0,0 +1,9 @@
+package com.emarte.regurgitator.extensions.web;
+
+import com.emarte.regurgitator.test.Mock;
+
+public class HttpClientWrapper extends Mock {
+	public HttpClientWrapper(String host, int port, String username, String password) {
+		super(host, port, username, password);
+	}
+}
--- src/main/java/com/emarte/regurgitator/extensions/web/HttpMessageProxy.java
+++ src/main/java/com/emarte/regurgitator/extensions/web/HttpMessageProxy.java
@@ -3,7 +3,7 @@ package com.emarte.regurgitator.extensions.web;
 import com.emarte.regurgitator.test.Mock;
 
 public class HttpMessageProxy extends Mock {
-	public HttpMessageProxy(String host, int port, String username, String password) {
-		super(host, port, username, password);
+	public HttpMessageProxy(HttpClientWrapper wrapper) {
+		super(wrapper);
 	}
 }
