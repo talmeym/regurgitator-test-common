44e775fe7d8a069d4eacc8ff95add581de068426
talmeym <mtalmey@gmail.com>
2016-02-17 11:40:19 +0000

    added last-index flag to size processor

--- src/main/java/com/emarte/regurgitator/core/SizeProcessor.java
+++ src/main/java/com/emarte/regurgitator/core/SizeProcessor.java
@@ -3,4 +3,7 @@ package com.emarte.regurgitator.core;
 import com.emarte.regurgitator.test.Mock;
 
 public class SizeProcessor extends Mock implements ValueProcessor {
+	public SizeProcessor(boolean lastIndex) {
+		super(lastIndex);
+	}
 }
