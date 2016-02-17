2ae286213ea8e0b59067baf659ee3f979fd78b3d
talmeym <mtalmey@gmail.com>
2016-02-17 14:48:44 +0000

    added fullLoad test for missing xml loaders

--- src/main/java/com/emarte/regurgitator/extensions/FreemarkerProcessor.java
+++ src/main/java/com/emarte/regurgitator/extensions/FreemarkerProcessor.java
@@ -1,8 +1,9 @@
 package com.emarte.regurgitator.extensions;
 
+import com.emarte.regurgitator.core.ValueProcessor;
 import com.emarte.regurgitator.test.Mock;
 
-public class FreemarkerProcessor extends Mock {
+public class FreemarkerProcessor extends Mock implements ValueProcessor {
 	public FreemarkerProcessor(String value) {
 		super(value);
 	}
--- src/main/java/com/emarte/regurgitator/extensions/VelocityProcessor.java
+++ src/main/java/com/emarte/regurgitator/extensions/VelocityProcessor.java
@@ -1,8 +1,9 @@
 package com.emarte.regurgitator.extensions;
 
+import com.emarte.regurgitator.core.ValueProcessor;
 import com.emarte.regurgitator.test.Mock;
 
-public class VelocityProcessor extends Mock {
+public class VelocityProcessor extends Mock implements ValueProcessor {
 	public VelocityProcessor(String value) {
 		super(value);
 	}
