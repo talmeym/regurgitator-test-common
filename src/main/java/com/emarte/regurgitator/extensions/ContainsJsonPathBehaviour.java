447a5f7b33e673fbb8a853e19afafe35ad4de289
talmeym <mtalmey@gmail.com>
2016-03-02 17:49:59 +0000

    Renamed ContainsJsonPath

--- src/main/java/com/emarte/regurgitator/extensions/ContainsJsonPathBehaviour.java
+++ src/main/java/com/emarte/regurgitator/extensions/ContainsJsonPathBehaviour.java
@@ -3,5 +3,5 @@ package com.emarte.regurgitator.extensions;
 import com.emarte.regurgitator.core.ConditionBehaviour;
 import com.emarte.regurgitator.test.Mock;
 
-public class ContainsJsonPath extends Mock implements ConditionBehaviour {
+public class ContainsJsonPathBehaviour extends Mock implements ConditionBehaviour {
 }
