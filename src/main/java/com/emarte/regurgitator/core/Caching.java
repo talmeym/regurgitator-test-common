d3c1601fb5c6f4eab4a8a1d45799b5c257844082
talmeym <mtalmey@gmail.com>
2016-03-09 10:23:31 +0000

    tidied logging and imports

--- src/main/java/com/emarte/regurgitator/core/Caching.java
+++ src/main/java/com/emarte/regurgitator/core/Caching.java
@@ -1,9 +1,10 @@
 package com.emarte.regurgitator.core;
 
 import static com.emarte.regurgitator.core.CacheProvider.Cache;
+import static com.emarte.regurgitator.core.Log.getLog;
 
 public class Caching {
-	private static final Log log = Log.getLog(Caching.class);
+	private static final Log log = getLog(Caching.class);
 	private static final CacheProvider PROVIDER = new DefaultCacheProvider();
 
 	public static <TYPE> Cache<TYPE> getCache(Class<TYPE> clazz) {
