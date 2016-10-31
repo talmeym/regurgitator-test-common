574d165aa88b475d6af2ad4b37a9a266126738f9
talmeym <mtalmey@gmail.com>
2016-10-31 14:07:54 +0000

    changed ordering of hashmap contents in expectations to suite new jvm version

---
+++ src/main/java/com/emarte/regurgitator/core/RecordMessage.java
@@ -0,0 +1,10 @@
+package com.emarte.regurgitator.core;
+
+import com.emarte.regurgitator.test.MockStep;
+
+class RecordMessage extends MockStep {
+
+	public RecordMessage(Object id, String folderPath) {
+		super(id, folderPath);
+	}
+}
