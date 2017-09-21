75e55a0f34155e8a13faaa1f0d9ac47936489a61
talmeym <mtalmey@gmail.com>
2017-09-21 11:12:32 +0100

    renamed FileResponse CreateFileResponse

--- src/main/java/com/emarte/regurgitator/extensions/web/CreateFileResponse.java
+++ src/main/java/com/emarte/regurgitator/extensions/web/CreateFileResponse.java
@@ -3,8 +3,8 @@ package com.emarte.regurgitator.extensions.web;
 import com.emarte.regurgitator.core.ValueSource;
 import com.emarte.regurgitator.test.*;
 
-public class FileResponse extends MockStep {
-	public FileResponse(Object id, ValueSource valueSource, String pathPrefix) {
+public class CreateFileResponse extends MockStep {
+	public CreateFileResponse(Object id, ValueSource valueSource, String pathPrefix) {
 		super(id, valueSource, pathPrefix);
 	}
 }
