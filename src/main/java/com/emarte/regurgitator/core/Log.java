299bdef032484b823a22797eb599ab774bdfc8ec
talmeym <mtalmey@gmail.com>
2017-10-07 14:45:53 +0100

    Applied MIT License source comments

--- src/main/java/com/emarte/regurgitator/core/Log.java
+++ src/main/java/com/emarte/regurgitator/core/Log.java
@@ -1,18 +1,19 @@
+/*
+ * Copyright (C) 2017 Miles Talmey.
+ * Distributed under the MIT License (license terms are at http://opensource.org/licenses/MIT).
+ */
 package com.emarte.regurgitator.core;
 
 public class Log {
-    private Class clazz;
+    private final Class clazz;
 
     private Log(Class clazz) {
         this.clazz = clazz;
     }
 
     public void debug(String message, Object... objs) {
-        System.out.println(String.format(buildLogEntry(message), objs));
-    }
-
-    private String buildLogEntry(String message) {
-        return "DEBUG [" + clazz.getName() + "] " + message;
+        String logEntry = "DEBUG [" + clazz.getName() + "] " + message;
+        System.out.println(String.format(logEntry.replace("{}", "%s"), objs));
     }
 
     public static Log getLog(Class clazz){
