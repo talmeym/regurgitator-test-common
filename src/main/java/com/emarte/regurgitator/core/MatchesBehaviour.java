ab8f9c1cac8d5f873d0577d8b1a084dcba257ab5
talmeym <mtalmey@gmail.com>
2018-04-05 20:30:07 +0100

    added matches behaviour mock for loader testing

---
+++ src/main/java/com/emarte/regurgitator/core/MatchesBehaviour.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright (C) 2017 Miles Talmey.
+ * Distributed under the MIT License (license terms are at http://opensource.org/licenses/MIT).
+ */
+package com.emarte.regurgitator.core;
+
+import com.emarte.regurgitator.test.Mock;
+
+public final class MatchesBehaviour extends Mock implements ConditionBehaviour {
+}
