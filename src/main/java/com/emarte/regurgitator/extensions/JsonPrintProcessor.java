79f645693e2bcafe81a811c87153a04a0d677b2f
talmeym <mtalmey@gmail.com>
2019-05-23 09:04:02 +0100

    added json path processor

---
+++ src/main/java/com/emarte/regurgitator/extensions/JsonPrintProcessor.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (C) 2017 Miles Talmey.
+ * Distributed under the MIT License (license terms are at http://opensource.org/licenses/MIT).
+ */
+package com.emarte.regurgitator.extensions;
+
+import com.emarte.regurgitator.core.ValueProcessor;
+import com.emarte.regurgitator.test.Mock;
+
+public class JsonPrintProcessor extends Mock implements ValueProcessor {
+}
