a7da7ac5068c8b5fb82a3e58c1444597f398f12e
talmeym <mtalmey@gmail.com>
2019-06-05 10:52:43 +0100

    added at index processor

--- src/main/java/com/emarte/regurgitator/core/AtIndexProcessor.java
+++ src/main/java/com/emarte/regurgitator/core/AtIndexProcessor.java
@@ -6,8 +6,8 @@ package com.emarte.regurgitator.core;
 
 import com.emarte.regurgitator.test.Mock;
 
-public class IndexProcessor extends Mock implements ValueProcessor {
-    public IndexProcessor(ValueSource valueSource) {
-        super(valueSource);
+public class AtIndexProcessor extends Mock implements ValueProcessor {
+    public AtIndexProcessor(ValueSource indexSource) {
+        super(indexSource);
     }
 }
