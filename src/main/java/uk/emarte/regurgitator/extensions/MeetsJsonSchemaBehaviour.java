81a807d8b4b9866af98be73cb1e9ff64e72e92f7
talmeym <mtalmey@gmail.com>
2024-01-23 00:11:25 +0000

    added meets schema behaviours, 0.1.3

---
+++ src/main/java/uk/emarte/regurgitator/extensions/MeetsJsonSchemaBehaviour.java
@@ -0,0 +1,7 @@
+package uk.emarte.regurgitator.extensions;
+
+import uk.emarte.regurgitator.core.ConditionBehaviour;
+import uk.emarte.regurgitator.test.Mock;
+
+public class MeetsJsonSchemaBehaviour extends Mock implements ConditionBehaviour {
+}
---
+++ src/main/java/uk/emarte/regurgitator/extensions/MeetsXmlSchemaBehaviour.java
@@ -0,0 +1,7 @@
+package uk.emarte.regurgitator.extensions;
+
+import uk.emarte.regurgitator.core.ConditionBehaviour;
+import uk.emarte.regurgitator.test.Mock;
+
+public class MeetsXmlSchemaBehaviour extends Mock implements ConditionBehaviour {
+}
